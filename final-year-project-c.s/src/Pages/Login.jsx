e0926b2c46a5a39575b9d069aa551f4a390364ca
eyueldegefa <you@example.com>
2024-11-25 00:04:18 +0300

    Starting Authentication

--- final-year-project-c.s/src/Pages/Login.jsx
+++ final-year-project-c.s/src/Pages/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { auth } from '../Firebase.js';
 
 function Login() {
   return (
+    
     <div>
+      {/* // Should log the auth instance if Firebase is set up correctly */}
+      console.log(auth); 
+      Firebase Setup Complete!
     </div>
   )
 }
