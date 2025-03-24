db3be20be8875b28108a738e2df1d856275de6bf
eyueldegefa <you@example.com>
2025-03-24 10:27:35 +0300

    Update

--- final-year-project-c.s/src/Components/Banner/Banner.jsx
+++ final-year-project-c.s/src/Components/Banner/Banner.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import bannerSmalls from '../../Images/Banner1.jpg';
 import bannerLarges from '../../Images/banner2.jpg';
-import './Banner.css'
+import './Banner.module.css'
 
 function Banner() {
   return (
     <div className='banner'>
-      {/* <img className='bannerSmall d-md-none' src={bannerSmalls} alt="#" /> */}
-      <img className='bannerLarge' src={bannerLarges} alt="#" />
+      <img className='bannerSmall d-md-none' src={bannerSmalls} alt="#" />
+      <img className='d-none d-md-block bannerLarge' src={bannerLarges} alt="#" />
     </div>
   )
 }
